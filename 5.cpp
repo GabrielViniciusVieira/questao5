66e9646a5eea46839342561d9755b9e4fcf5d042
Gabriel Vinícius <69484832+GabrielViniciusVieira@users.noreply.github.com>
2020-12-15 23:43:49 -0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -2,29 +2,38 @@ void insere(const T& elemento, const int posicao){
     Celula<T>* aux1 = NULL
     Celula<T>* aux2 = this->inicio;
     int i = 0;
-    while( (aux2 != NULL) && (posicao != i) ) {
+    while( (aux2 != NULL) && (posicao != i) )  
+        {
         aux1 = aux2;
         aux2 = aux2->prox;
         i++;
-    }
-    if(aux1 == NULL && aux2 == NULL) { //lista vazia
-        if(indice == 0) {
+        }
+    if(aux1 == NULL && aux2 == NULL) //lista vazia
+    { 
+        if(indice == 0) 
+            {
             Celula<T>* c = new Celula<T>(elemento);
             c->prox = NULL;
             this->inicio = c;
             this->fim = c;
-        } else {
-            cout << "Elemento nao inserido!" << endl;
-        }
-    } else if(aux1 == NULL) { //indice 0
+            } 
+        else  
+        cout << "Elemento nao inserido!" << endl;
+    } 
+    
+    else if(aux1 == NULL) //indice 0
+        { //indice 0
         Celula<T>* c = new Celula<T>(elemento);
         c->prox = NULL;
         this->inicio = c;
-    } else if(aux2 == NULL) {
+        } 
+    else if(aux2 == NULL) 
         cout << "Elemento nao inserido!" << endl;
-    } else {
+        
+    else 
+        {
         Celula<T>* c = new Celula<T>(elemento);
         aux1->prox = c;
         c->prox = aux2;
-    }
-}
+        }
+                                                }
